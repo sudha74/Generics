e42ceb6ad60bc17eb78635f02c82e74463d7f6ed
sudha123 <sudhanaukala1997@gmail.com>
2022-07-26 19:35:05 +0530

    create generic class to take variables of generic type

--- src/com/bridgelabz/TestMax.java
+++ src/com/bridgelabz/TestMax.java
@@ -1,6 +1,16 @@
 package com.bridgelabz;
 
-public class TestMax {
+public class TestMax <T extends Comparable<T>> {
+    T x,y,z;
+
+    TestMax(T x, T y,T z){
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+    public T maximum(){
+        return TestMax.maximum(x,y,z);
+    }
 
     public static <T extends Comparable<T>> T maximum(T x, T y, T z){
         T max = x;
@@ -15,9 +25,11 @@ public class TestMax {
     }
 
     public static void main(String[] args) {
-        maximum(50,90,20);
-        maximum(50.55,90.99,20.22);
-        maximum("Peach","Banana","Apple");
-
+        Integer xInt = 50, yInt = 90, zInt = 20;
+        Float xFloat = 50.55f, yFloat = 90.99f, zFloat = 20.22f;
+        String xString = "Peach", yString = "Banana", zString = "Apple";
+        new TestMax(xInt,yInt,zInt).maximum();
+        new TestMax(xFloat,yFloat,zFloat).maximum();
+        new TestMax(xString,yString,zString).maximum();
     }
 }
