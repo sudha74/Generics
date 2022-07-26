c48b36b09d2f1e28be973693003ea0744ef47ae4
sudha123 <sudhanaukala1997@gmail.com>
2022-07-26 19:13:31 +0530

    maximum number at first position

--- src/com/bridgelabz/TestMax.java
+++ src/com/bridgelabz/TestMax.java
@@ -1,14 +1,15 @@
+package com.bridgelabz;
 
 public class TestMax {
 
     public static void main(String[] args) {
-        int a = 50;
-        int b = 90;
+        int a = 90; //90, 50, 20
+        int b = 50;
         int c = 20;
         int temp;
         int temp1;
         temp = a > b ? a:b;
         temp1 = temp > c ? temp:c;
-        System.out.println(temp1);
+        System.out.println("maximum number among 3 integer: "+temp1);
     }
 }
