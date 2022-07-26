0f7b57019685c4f31d513cd39f72d9df478ae3cc
sudha123 <sudhanaukala1997@gmail.com>
2022-07-26 19:29:08 +0530

    find max in one generic method

--- src/com/bridgelabz/TestMax.java
+++ src/com/bridgelabz/TestMax.java
@@ -2,19 +2,22 @@ package com.bridgelabz;
 
 public class TestMax {
 
-    public static void main(String[] args) {
-        String a = "Peach";
-        String b = "Apple";
-        String c = "Banana";
-        String max;
+    public static <T extends Comparable<T>> T maximum(T x, T y, T z){
+        T max = x;
+        if(y.compareTo(max) > 0){
+            max = y;
+        }
+        if (z.compareTo(max) > 0){
+            max = z;
+        }
+        System.out.printf("Max of %s, %s and %s is: %s\n",x,y,z,max);
+        return max;
+    }
 
-        max = a;
-        if (b.compareTo(max)>0){
-            max = b;
-        }
-        if (c.compareTo(max)>0){
-            max = c;
-        }
-        System.out.println("maximum string value among 'Peach','Apple','Banana' is: "+max);
+    public static void main(String[] args) {
+        maximum(50,90,20);
+        maximum(50.55,90.99,20.22);
+        maximum("Peach","Banana","Apple");
+
     }
 }
