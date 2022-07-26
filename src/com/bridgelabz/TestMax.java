d32cd83c604b6d9578113f246266c1e6ecc71f46
sudha123 <sudhanaukala1997@gmail.com>
2022-07-26 19:25:47 +0530

    find three strings as max

--- src/com/bridgelabz/TestMax.java
+++ src/com/bridgelabz/TestMax.java
@@ -3,13 +3,18 @@ package com.bridgelabz;
 public class TestMax {
 
     public static void main(String[] args) {
-        float a = 50.55f; //50, 90, 20
-        float b = 20.22f;
-        float c = 90.99f;
-        float temp;
-        float temp1;
-        temp = a > b ? a:b;
-        temp1 = temp > c ? temp:c;
-        System.out.println("maximum number among 3 float number: "+temp1);
+        String a = "Peach";
+        String b = "Apple";
+        String c = "Banana";
+        String max;
+
+        max = a;
+        if (b.compareTo(max)>0){
+            max = b;
+        }
+        if (c.compareTo(max)>0){
+            max = c;
+        }
+        System.out.println("maximum string value among 'Peach','Apple','Banana' is: "+max);
     }
 }
